5c432ae9e8702567c108f386f692206ab30a6b5d
JRKy <josh.kennedy@gmail.com>
2025-03-19 22:52:38 -0600

    Create data.js

---
+++ data.js
@@ -0,0 +1,10 @@
+const SATELLITES = [
+    { name: "ALT-3", longitude: -150 },
+    { name: "MUOS-4", longitude: -100 },
+    { name: "WGS-10", longitude: -45 },
+    { name: "AEHF-6", longitude: 5 },
+    { name: "SES-20", longitude: 60 },
+    { name: "INTELSAT-39", longitude: 105 }
+];
+
+console.log(`Satellite Data Loaded: ${SATELLITES.length} satellites`);
