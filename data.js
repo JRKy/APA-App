2b4ff092956d844ee6d63e4f9c1c2cb1421ca7d8
JRKy <josh.kennedy@gmail.com>
2025-03-23 09:48:15 -0600

    Update data.js

--- data.js
+++ data.js
@@ -1,18 +1,19 @@
-const SATELLITES = [
-    { name: "ALT-3", longitude: -150 },
-    { name: "MUOS-4", longitude: -100 },
-    { name: "WGS-10", longitude: -45 },
-    { name: "AEHF-6", longitude: 5 },
-    { name: "SES-20", longitude: 60 },
-    { name: "INTELSAT-39", longitude: 105 }
-];
+// data.js - v1.5.7
 
 const LOCATIONS = [
-    { name: "Busan", latitude: 35.1796, longitude: 129.0756 },
-    { name: "Cape Canaveral", latitude: 28.3922, longitude: -80.6077 },
-    { name: "Los Angeles", latitude: 34.0522, longitude: -118.2437 },
-    { name: "London", latitude: 51.5074, longitude: -0.1278 },
-    { name: "Sydney", latitude: -33.8688, longitude: 151.2093 }
+  { name: "Busan", latitude: 35.1796, longitude: 129.0756, country: "South Korea", aor: "USINDOPACOM" },
+  { name: "Ramstein AB", latitude: 49.4369, longitude: 7.6003, country: "Germany", aor: "USEUCOM" },
+  { name: "Camp Lemonnier", latitude: 11.5475, longitude: 43.1598, country: "Djibouti", aor: "USAFRICOM" },
+  { name: "Al Udeid AB", latitude: 25.1172, longitude: 51.3150, country: "Qatar", aor: "USCENTCOM" },
+  { name: "Peterson SFB", latitude: 38.8258, longitude: -104.7005, country: "United States", aor: "USNORTHCOM" },
+  { name: "Joint Base Pearl Harbor-Hickam", latitude: 21.3469, longitude: -157.9300, country: "United States", aor: "USINDOPACOM" }
 ];
 
-console.log(`Data Loaded: ${SATELLITES.length} satellites, ${LOCATIONS.length} locations`);
+const SATELLITES = [
+  { name: "ALT-3", longitude: 15 },
+  { name: "MUOS-4", longitude: 75 },
+  { name: "WGS-1", longitude: -135 },
+  { name: "WGS-10", longitude: 105 },
+  { name: "AEHF-6", longitude: -11 },
+  { name: "GALAXY-30", longitude: -125 }
+];
