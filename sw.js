fd41a069c6924fd84375aee9d93c988d0df6e6db
JRKy <josh.kennedy@gmail.com>
2025-03-23 12:01:16 -0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,17 +1,17 @@
-const CACHE_NAME = "apa-cache-v1.6.7";
+const CACHE_NAME = "apa-cache-v1.6.8";
 const urlsToCache = [
   "./",
   "index.html",
-  "styles.css?v=1.6.7",
-  "data.js?v=1.6.7",
-  "script.js?v=1.6.7",
+  "styles.css?v=1.6.8",
+  "data.js?v=1.6.8",
+  "script.js?v=1.6.8",
   "manifest.json",
   "icons/icon-192.png",
   "icons/icon-512.png"
 ];
 
 self.addEventListener("install", event => {
-  console.log("Installed SW Version: v1.6.7");
+  console.log("Installed SW Version: v1.6.8");
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
   );
