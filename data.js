8e2ca0484768389f6bd79db73132421426d2d071
JRKy <josh.kennedy@gmail.com>
2025-03-23 09:56:09 -0600

    Update data.js

--- data.js
+++ data.js
@@ -1,12 +1,27 @@
-// data.js - v1.5.7
+// data.js - v1.5.8
 
 const LOCATIONS = [
-  { name: "Busan", latitude: 35.1796, longitude: 129.0756, country: "South Korea", aor: "USINDOPACOM" },
-  { name: "Ramstein AB", latitude: 49.4369, longitude: 7.6003, country: "Germany", aor: "USEUCOM" },
-  { name: "Camp Lemonnier", latitude: 11.5475, longitude: 43.1598, country: "Djibouti", aor: "USAFRICOM" },
-  { name: "Al Udeid AB", latitude: 25.1172, longitude: 51.3150, country: "Qatar", aor: "USCENTCOM" },
-  { name: "Peterson SFB", latitude: 38.8258, longitude: -104.7005, country: "United States", aor: "USNORTHCOM" },
-  { name: "Joint Base Pearl Harbor-Hickam", latitude: 21.3469, longitude: -157.9300, country: "United States", aor: "USINDOPACOM" }
+  { name: "NSA Bahrain", latitude: 26.2076027857257, longitude: 50.6111061537939, country: "Bahrain", aor: "USCENTCOM" },
+  { name: "Camp Arifjan", latitude: 28.88745959804, longitude: 48.1699731354169, country: "Kuwait", aor: "USCENTCOM" },
+  { name: "Al Udeid AB", latitude: 25.1235678538811, longitude: 51.3374629203215, country: "Qatar", aor: "USCENTCOM" },
+  { name: "MacDill AFB, FL", latitude: 27.8510433301987, longitude: -82.5090662423147, country: "United States", aor: "USNORTHCOM" },
+  { name: "Ramstein AB", latitude: 49.4401704826424, longitude: 7.59720948275646, country: "Germany", aor: "USEUCOM" },
+  { name: "Clay Kaserne", latitude: 50.0407041208674, longitude: 8.32711774313159, country: "Germany", aor: "USEUCOM" },
+  { name: "Panzer Kaserne", latitude: 48.6858355365503, longitude: 9.04351944045798, country: "Germany", aor: "USEUCOM" },
+  { name: "Patch Barracks", latitude: 48.7359556828308, longitude: 9.08381048452004, country: "Germany", aor: "USEUCOM" },
+  { name: "Kelley Barracks", latitude: 48.7219919385823, longitude: 9.18093129807744, country: "Germany", aor: "USEUCOM" },
+  { name: "Yokota AB", latitude: 35.7376122917397, longitude: 139.343845833246, country: "Japan", aor: "USINDOPACOM" },
+  { name: "Camp Zama", latitude: 35.4897327860585, longitude: 139.395765984187, country: "Japan", aor: "USINDOPACOM" },
+  { name: "CP Tango", latitude: 37.5168786865521, longitude: 126.983267811205, country: "South Korea", aor: "USINDOPACOM" },
+  { name: "Camp Humphreys", latitude: 36.9663892737021, longitude: 127.009713317196, country: "South Korea", aor: "USINDOPACOM" },
+  { name: "Osan AB", latitude: 37.0831716293535, longitude: 127.034182452515, country: "South Korea", aor: "USINDOPACOM" },
+  { name: "Busan", latitude: 35.1641114151272, longitude: 129.055238532993, country: "South Korea", aor: "USINDOPACOM" },
+  { name: "Camp Smith, HI", latitude: 21.3854942009106, longitude: -157.907935527315, country: "United States", aor: "USINDOPACOM" },
+  { name: "CMSFS, CO", latitude: 38.859055, longitude: -104.813499, country: "United States", aor: "USSPACECOM" },
+  { name: "Peterson SFB, CO", latitude: 38.0, longitude: -104.0, country: "United States", aor: "USSPACECOM" },
+  { name: "Omaha AFB, NE", latitude: 41.1183, longitude: -95.9052, country: "United States", aor: "USSTRATCOM" },
+  { name: "RRMC, PA", latitude: 36.233402, longitude: -91.251801, country: "United States", aor: "USNORTHCOM" },
+  { name: "Schriever SFB, CO", latitude: 29.74215, longitude: -90.81037, country: "United States", aor: "USSPACECOM" }
 ];
 
 const SATELLITES = [
